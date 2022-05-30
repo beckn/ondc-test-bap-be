ee64298f32afde75c9156e5f7fda8f370b880677
Rajat Kumar <er.rjtkumar@gmail.com>
2022-05-30 21:09:46 +0530

    Added Logs in Search Polling

--- biab-bap-client/src/main/kotlin/org/beckn/one/sandbox/bap/client/discovery/controllers/OnSearchPollController.kt
+++ biab-bap-client/src/main/kotlin/org/beckn/one/sandbox/bap/client/discovery/controllers/OnSearchPollController.kt
@@ -10,6 +10,8 @@ import org.beckn.one.sandbox.bap.factories.ContextFactory
 import org.beckn.one.sandbox.bap.factories.LoggingFactory
 import org.beckn.protocol.schemas.ProtocolContext
 import org.beckn.protocol.schemas.ProtocolOnSearch
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMapping
@@ -25,11 +27,17 @@ class OnSearchPollController @Autowired constructor(
   loggingFactory: LoggingFactory,
   loggingService: LoggingService,
 ) : AbstractOnPollController<ProtocolOnSearch, ClientSearchResponse>(onPollService, contextFactory, loggingFactory, loggingService) {
+  private val log: Logger = LoggerFactory.getLogger(this::class.java)
 
   @RequestMapping("/client/v1/on_search")
   @ResponseBody
   fun onSearchV1(@RequestParam messageId: String,
                  @RequestParam providerName: String?,
-                 @RequestParam categoryName: String?): ResponseEntity<out ClientResponse> = onPoll(messageId,
-    protocolClient.getSearchResponsesCall(messageId, providerName, categoryName), ProtocolContext.Action.ON_SEARCH)
+                 @RequestParam categoryName: String?): ResponseEntity<out ClientResponse> {
+    log.info("on Search for client layer : $messageId, $providerName, $categoryName")
+   return onPoll(
+      messageId,
+      protocolClient.getSearchResponsesCall(messageId, providerName, categoryName), ProtocolContext.Action.ON_SEARCH
+    )
+  }
 }
