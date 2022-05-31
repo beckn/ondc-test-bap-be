3b5cd7412254164455be4cecb6b1b19da131dbcb
Rajat Kumar <er.rjtkumar@gmail.com>
2022-05-31 23:40:43 +0530

    Update BecknResponseRepository.kt

--- biab-bap-protocol/src/main/kotlin/org/beckn/one/sandbox/bap/message/repositories/BecknResponseRepository.kt
+++ biab-bap-protocol/src/main/kotlin/org/beckn/one/sandbox/bap/message/repositories/BecknResponseRepository.kt
@@ -1,11 +1,11 @@
 package org.beckn.one.sandbox.bap.message.repositories
 
-import arrow.core.andThen
-import arrow.optics.cons
 import com.mongodb.client.MongoCollection
 import org.beckn.one.sandbox.bap.message.entities.*
-import org.beckn.one.sandbox.bap.protocol.shared.Util.snakeToUpperCamelCase
-import org.litote.kmongo.*
+import org.litote.kmongo.and
+import org.litote.kmongo.div
+import org.litote.kmongo.eq
+import org.litote.kmongo.regex
 
 open class BecknResponseRepository<R : BecknResponseDao>(
   collection: MongoCollection<R>
@@ -14,6 +14,7 @@ open class BecknResponseRepository<R : BecknResponseDao>(
   fun findByMessageId(id: String): List<R> = findAll(BecknResponseDao::context / ContextDao::messageId eq id)
   fun findByOrderId(id: String) : List<R> = findAll(OnOrderStatusDao::message / OnOrderStatusMessageDao::order / OrderDao::id eq id)
   fun findByCategoryName(id: String, categoryName: String) : List<R> = findAll(BecknResponseDao::context / ContextDao::messageId eq id)
-  fun findByProviderName(id: String, providerName: String) : List<R> = findAll(and(OnSearchDao::context / ContextDao::messageId eq id, OnSearchDao::message/ OnSearchMessageDao::catalog / CatalogDao::bppDescriptor/ DescriptorDao::name  regex providerName.snakeToUpperCamelCase()))
+  fun findByProviderName(id: String, providerName: String) : List<R> = findAll(and(OnSearchDao::context / ContextDao::messageId eq id,
+    OnSearchDao::message/ OnSearchMessageDao::catalog / CatalogDao::bppDescriptor/ DescriptorDao::name regex providerName.toRegex(RegexOption.IGNORE_CASE)))
 
 }
