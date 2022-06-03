68b50269c77a3198f4d9392064dd190256a9adce
Rajat Kumar <er.rjtkumar@gmail.com>
2022-06-03 22:09:08 +0530

    updated firebase configuration

--- biab-bap-client/src/main/kotlin/org/beckn/one/sandbox/bap/configurations/FirebaseConfiguration.kt
+++ biab-bap-client/src/main/kotlin/org/beckn/one/sandbox/bap/configurations/FirebaseConfiguration.kt
@@ -6,18 +6,23 @@ import java.io.IOException
 import com.google.firebase.FirebaseOptions
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.io.InputStream
 
 @Configuration
-class FirebaseConfiguration {
-    @Primary
+class FirebaseConfiguration (
+  @Value("\${firebase.config}") private val firebaseConfigName: String
+  ) {
+
+
+  @Primary
     @Bean
     fun firebaseInit() {
         var inputStream: InputStream? = null
         try {
-            inputStream = ClassPathResource("firebase_config.json").inputStream
+            inputStream = ClassPathResource(firebaseConfigName).inputStream
         } catch (e3: IOException) {
             e3.printStackTrace()
         }
